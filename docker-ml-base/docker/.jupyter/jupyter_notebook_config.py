9baa29de95336cc87dbd8a52c8dd80aabb8e5b66
shitake <p230584300813995212@gmail.com>
2019-12-03 02:35:23 +0900

    docker-ml-base: コンテナ起動時にJupyterも立ち上がるよう設定

---
+++ docker-ml-base/docker/.jupyter/jupyter_notebook_config.py
@@ -0,0 +1,15 @@
+import os
+
+c = get_config()
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.notebook_dir = '/home/sandbox/notebooks'
+
+# https://github.com/jupyter/notebook/issues/3130
+c.FileContentsManager.delete_to_trash = False
+
+# Change default umask for all subprocesses of the notebook server if set in
+# the environment
+if 'NB_UMASK' in os.environ:
+    os.umask(int(os.environ['NB_UMASK'], 8))
---
+++ docker-ml-base/docker/entrypoint.sh
@@ -0,0 +1,1 @@
+jupyter notebook --config=/.jupyter/jupyter_notebook_config.py --ip=0.0.0.0 --allow-root
